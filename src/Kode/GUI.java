4a7072ded5244db47c3e6dd159105b88cbf6f773
Henrik <Eier@158.38.54.31>
2012-03-15 15:26:17 +0100

    Fiksa stygg menubar

--- src/Kode/GUI.java
+++ src/Kode/GUI.java
@@ -1,16 +1,14 @@
 package Kode;
 
 import java.awt.BorderLayout;
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JInternalFrame;
-import javax.swing.JPanel;
+import java.awt.Dimension;
+import javax.swing.*;
 
 public class GUI extends JFrame{
-    private JButton knapp = new JButton("LOOOL");
-    private JButton knapp2 = new JButton("LOOOL");
-    private JButton knapp3 = new JButton("LOOOL");
-    private JButton knapp4 = new JButton("LOOOL");
+    private JLabel knapp = new JLabel(new ImageIcon("src/Kode/Bilder/north.png"));
+    private JLabel knapp2 = new JLabel(new ImageIcon("src/Kode/Bilder/east.png"));
+    private JLabel knapp3 = new JLabel(new ImageIcon("src/Kode/Bilder/west.png"));
+    private JLabel knapp4 = new JLabel(new ImageIcon("src/Kode/Bilder/south.png"));
     
     public GUI(String tittel) {
         setTitle(tittel);
@@ -19,9 +17,9 @@ public class GUI extends JFrame{
         JInternalFrame a = new Sjakk();
         frame.add(a);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.add(knapp2, BorderLayout.SOUTH);
+        frame.add(knapp4, BorderLayout.SOUTH);
         frame.add(knapp3, BorderLayout.WEST);
-        frame.add(knapp4, BorderLayout.EAST);
+        frame.add(knapp2, BorderLayout.EAST);
         frame.add(knapp, BorderLayout.NORTH);
         frame.pack();
         frame.setLocationRelativeTo(null);
@@ -30,6 +28,7 @@ public class GUI extends JFrame{
     }
     public static void main(String[] args){
         GUI gui = new GUI("Sjakk");
+        gui.setSize(1280, 800);
         
     }
 }
