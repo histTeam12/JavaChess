d6c7aca0dfa9e4894d9cfd0cba726d9f39e9fb89
Eier <Eier@158.38.54.31>
2012-03-15 12:55:54 +0100

    test

--- src/Kode/Sjakk.java
+++ src/Kode/Sjakk.java
@@ -8,7 +8,6 @@ import javax.swing.*;
 
 public class Sjakk extends JFrame implements MouseListener, MouseMotionListener {
     
-    int penis;
     int tur = 2;
     JLayeredPane layeredPane;
     BrettRute chessBoard;
