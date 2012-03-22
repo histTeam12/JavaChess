4de8011f3bc08a5e7fd1c4be814d98917926e435
Michael <Michael@158.38.31.220>
2012-03-22 15:09:40 +0100

    Avslutt funksjon

--- src/Kode/Menybar.java
+++ src/Kode/Menybar.java
@@ -1,7 +1,3 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package Kode;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
@@ -14,48 +10,50 @@ import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import javax.swing.*;
 
-class Menybar {
-    private GUI gui;
-public static JMenuBar createMenybar(){
+class Menybar extends JMenuBar {
+public Menybar(){
     //Lager Menybaren
-    JMenuBar menuBar = new JMenuBar();
     JMenu file = new JMenu("Fil");
     JMenu settings = new JMenu("Innstillinger");
     JMenu credits = new JMenu ("Credits");
     JMenu help = new JMenu ("Hjelp");
-    menuBar.add(file);
-    menuBar.add(settings);
-    menuBar.add(credits);
-    menuBar.add(help);
+    add(file);
+    add(settings);
+    add(credits);
+    add(help);
     //Lager knapper til Filmenyen
-    JMenuItem Item1 = new JMenuItem("Nytt Spill", 
-            new ImageIcon("src/Kode/Bilder/nyttspill1.png"));
-                Item1.setAccelerator(KeyStroke.getKeyStroke(
-                KeyEvent.VK_1, ActionEvent.SHIFT_MASK));
-    JMenuItem Item2 = new JMenuItem("Avslutt", 
-            new ImageIcon("src/Kode/Bilder/avslutt.png"));
+    JMenuItem Nyttspill = new JMenuItem("Nytt Spill", 
+        new ImageIcon("src/Kode/Bilder/nyttspill1.png"));
+        Nyttspill.setAccelerator(KeyStroke.getKeyStroke(
+        KeyEvent.VK_1, ActionEvent.SHIFT_MASK));
+    JMenuItem Avslutt = new JMenuItem("Avslutt", 
+        new ImageIcon("src/Kode/Bilder/avslutt.png"));
     JRadioButtonMenuItem Meme = new JRadioButtonMenuItem("Meme-sjakk");
     JRadioButtonMenuItem Vanlig = new JRadioButtonMenuItem("Vanlig sjakk");
+    JMenuItem Utviklere = new JMenuItem("Utviklere");
     ButtonGroup bg = new ButtonGroup();
     bg.add(Meme);
     bg.add(Vanlig);
-    file.add(Item1);
-    file.add(Item2);
+    file.add(Nyttspill);
+    file.add(Avslutt);
     settings.add(Meme);
     settings.add(Vanlig);
-    Item1.addActionListener(new ActionListener(){
-        public void actionPerformed(ActionEvent arg0){
-        //Her legges kommandoen for å restarte spillet.
+    credits.add(Utviklere);
+    Nyttspill.addActionListener(new ActionListener(){
+        public void actionPerformed(ActionEvent e){
+        
         }
     });
-    Item2.addActionListener(new ActionListener(){
-       public void actionPerformed(ActionEvent arg0){
+    Avslutt.addActionListener(new ActionListener(){
+        public void actionPerformed(ActionEvent e){
         System.exit(0);
        } 
     });
-    
-    
-    return menuBar;
+    Utviklere.addActionListener(new ActionListener(){
+        public void actionPerformed(ActionEvent e){
+        
+        }
+    });
 }
 
 }
