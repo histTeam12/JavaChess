84baf3059dccaf850b5dcfc256d016bce56fdbd9
Henrik <Eier@158.38.54.31>
2012-03-22 11:01:39 +0100

    Lagd logg-klasse

---
+++ src/Kode/Logg.java
@@ -0,0 +1,20 @@
+package Kode;
+
+import java.util.ArrayList;
+
+public class Logg {
+
+    ArrayList logg = new ArrayList();
+
+    public void leggTilLogg(String tekst) {
+        logg.add(tekst);
+    }
+
+    public String toString() {
+        String res = "";
+        for (int i = 0; i > logg.size(); i++) {
+            res += logg.get(i) + "\n";
+        }
+        return res;
+    }
+}
