1e2c9bacac04909802faea71cb46643de783d42a
Michael <Michael@158.38.31.220>
2012-03-22 13:35:07 +0100

    Avslutt funksjon

--- src/Kode/Menybar.java
+++ src/Kode/Menybar.java
@@ -15,17 +15,18 @@ import java.awt.event.KeyEvent;
 import javax.swing.*;
 
 class Menybar {
+    private GUI gui;
 public static JMenuBar createMenybar(){
     //Lager Menybaren
     JMenuBar menuBar = new JMenuBar();
     JMenu file = new JMenu("Fil");
-    JMenu Settings = new JMenu("Innstillinger");
-    JMenu Credits = new JMenu ("Credits");
-    JMenu Help = new JMenu ("Hjelp");
+    JMenu settings = new JMenu("Innstillinger");
+    JMenu credits = new JMenu ("Credits");
+    JMenu help = new JMenu ("Hjelp");
     menuBar.add(file);
-    menuBar.add(Settings);
-    menuBar.add(Credits);
-    menuBar.add(Help);
+    menuBar.add(settings);
+    menuBar.add(credits);
+    menuBar.add(help);
     //Lager knapper til Filmenyen
     JMenuItem Item1 = new JMenuItem("Nytt Spill", 
             new ImageIcon("src/Kode/Bilder/nyttspill1.png"));
@@ -33,18 +34,25 @@ public static JMenuBar createMenybar(){
                 KeyEvent.VK_1, ActionEvent.SHIFT_MASK));
     JMenuItem Item2 = new JMenuItem("Avslutt", 
             new ImageIcon("src/Kode/Bilder/avslutt.png"));
-    file.add(Item1);
-    file.add(Item2);
-    
     JRadioButtonMenuItem Meme = new JRadioButtonMenuItem("Meme-sjakk");
     JRadioButtonMenuItem Vanlig = new JRadioButtonMenuItem("Vanlig sjakk");
     ButtonGroup bg = new ButtonGroup();
     bg.add(Meme);
     bg.add(Vanlig);
-    Settings.add(Meme);
-    Settings.add(Vanlig);
-    
-    
+    file.add(Item1);
+    file.add(Item2);
+    settings.add(Meme);
+    settings.add(Vanlig);
+    Item1.addActionListener(new ActionListener(){
+        public void actionPerformed(ActionEvent arg0){
+        //Her legges kommandoen for å restarte spillet.
+        }
+    });
+    Item2.addActionListener(new ActionListener(){
+       public void actionPerformed(ActionEvent arg0){
+        System.exit(0);
+       } 
+    });
     
     
     return menuBar;
