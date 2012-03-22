774606bc2810416c4a8402543f7900824155cf5a
Henrik <Eier@158.38.54.31>
2012-03-22 15:10:00 +0100

    Lagd timer-klasse

---
+++ src/Kode/Timer.java
@@ -0,0 +1,37 @@
+package Kode;
+
+import javax.swing.*;
+import java.util.*;
+
+class Timer extends JLabel{
+    private int s;
+    private int m;
+    private int h;
+    
+    public Timer(){
+        
+        start();
+    }
+    private void reset(){
+        s = 0;
+        m = 0;
+        h = 0;
+    }
+    public void start(){
+        reset();
+        java.util.Timer timer = new java.util.Timer();
+        timer.scheduleAtFixedRate(new TimerTask(){
+            public void run(){
+                
+                if (s <= 59) s++;                                                                
+                if (s == 60 && m < 59) m++;                
+                if (s == 60 && m == 59) h++;
+                if (s == 60 && m == 60 && h == 60) h = 0;
+                if (s == 60 && m == 59) m = 0;
+                if (s == 60) s =0;                    
+                
+                setText( h+":"+m+":"+s );
+            }
+        }, 0, 1000 );
+    }
+}
