55d9c4e274ac026255376c7568832cf5c137da1d
Michael <Michael@158.38.31.220>
2012-03-22 16:05:32 +0100

    Avslutt funksjon

--- src/Kode/ScrollPane.java
+++ src/Kode/ScrollPane.java
@@ -4,18 +4,14 @@ import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 
 class ScrollPane {
-
 public static JScrollPane CreateScrollPane(){
-    JTextArea textArea = new JTextArea(5, 5);
+    JTextArea textArea = new JTextArea("Hello");
+    textArea.setEditable(false);
     JScrollPane liste = new JScrollPane(textArea);
     liste.setPreferredSize(new Dimension(200, 100));
     liste.createVerticalScrollBar();
     
     return liste;
 }
-//public static JScrollPane CreateCreditsPane(){
-  //  String tekst = "Hei";
-    //JScrollPane liste = new JScrollPane(tekst);
-    //liste.setPreferredSize(new Dimension(200,100));
 }
 
