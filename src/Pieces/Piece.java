fa46a9d584502951ef4c47f4baae4e45ecaa66af
Henrik <Eier@158.38.54.31>
2012-04-24 10:49:33 +0200

    Comments/javadocs på Piece

--- src/Pieces/Piece.java
+++ src/Pieces/Piece.java
@@ -5,9 +5,8 @@ import java.awt.Point;
 import java.io.Serializable;
 import javax.swing.Icon;
 
-//Super class for all the pieces 
 /**
- * 
+ * Super class for all the pieces 
  * @author andreaskalstad
  */
 abstract public class Piece implements Serializable {
@@ -33,9 +32,9 @@ abstract public class Piece implements Serializable {
      */
     public String name;
 
-    //Constructor
+    
     /**
-     * 
+     * Creates a new piece with the following icon
      * @param icon
      */
     public Piece(Icon icon) {
@@ -43,42 +42,52 @@ abstract public class Piece implements Serializable {
     }
     
     /**
-     * 
+     * Returns a String value of the name of this piece
      * @return
+     * Name of the piece
      */
     public String getName(){
         return name;
     }
     
     /**
-     * 
+     * Changes the icon representing this piece
      * @param icon
+     * the new icon
      */
     public void setIcon(Icon icon){
         this.icon = icon;
     }
 
-    //Method for checking the legal moves of a piece which is taken in as parameter
+    
     /**
-     * 
+     * Method for checking the legal moves of a piece which is taken in as parameter
      * @param y
+     * distance moved on the y axis in pixels
      * @param x
+     * distance moved on the x axis in pixels
      * @param start
+     * the pieces starting point
      * @param piece
+     * the piece being moved
      * @param team2
+     * the team this piece belongs to
      * @return
+     * true if legal move, else false
      */
     public abstract boolean legalMove(int y, int x, Point start, Object piece, int team2);
     
     /**
-     * 
+     * Grabs the icon representing this piece
      * @return
+     * Icon for this piece
      */
     public abstract Icon getIcon();
     
     /**
-     * 
+     * Returns the int value indicating wich team the piece belongs to
      * @return
+     * the value of this pieces team
      */
     public abstract int getTeam();
 }
